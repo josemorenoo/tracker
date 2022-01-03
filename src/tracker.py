2c72aac352082f3732df3685dec89a0b30c61fe7
Jose Moreno <jose.moreno@us.mcd.com>
2022-01-03 13:05:51 -0800

    dash test instead of matplotlib

--- src/tracker.py
+++ src/tracker.py
@@ -1,11 +1,19 @@
+# visit http://127.0.0.1:8050/ in your web browser.
+
 from datetime import datetime
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+
+
 from setup import load_data
-from HairyPlotter import HairyPlotter
+#from HairyPlotter import HairyPlotter
 
 
 if __name__ == "__main__":
     # setup
-    read_from_pickle = False
+    read_from_pickle = True
     token="LRC"
     start_date = datetime(2021, 12, 1, 12, 00, 00)
     end_date = datetime(2021, 12, 2, 12, 00, 00)
@@ -17,7 +25,7 @@ if __name__ == "__main__":
         'https://github.com/Loopring/whitepaper'
     ]
 
-    token_data, project_commits = load_data(
+    token_data_df, project_commits_list = load_data(
         token,
         project_repos,
         start_date,
@@ -27,6 +35,29 @@ if __name__ == "__main__":
         write_to_pickle = not read_from_pickle
     )
 
-    # it's levioSA
+    # create dash instance
+    dash_app = dash.Dash(__name__)
+    
+
+    # add price plot
+    fig = px.line(token_data_df, x='datetime', y="close")
+    # fig.add_trace(go.Scatter(mode="markers", x=df["Date"], y=df["AAPL.Close"], name="daily"))
+
+    dash_app.layout = html.Div([
+        dcc.Graph(
+            id='price-vs-some-metric',
+            figure=fig
+        )
+    ])
+
+    # this needs to run last AFTER you configure the dash_app object layout
+    dash_app.run_server(debug=True)
+
+    """
+    # matplotlib
     hp = HairyPlotter()
-    hp.show_commmit_plot(token_data, project_commits)
+    hp.show_commmit_plot(token_data_df, project_commits_list)
+    """
+
+
+    
