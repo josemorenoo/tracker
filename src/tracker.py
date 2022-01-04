0516cc671ad705bdabd6217190387c2ea93f0413
Jose Moreno <jose.moreno@us.mcd.com>
2022-01-04 06:22:56 -0800

    organize plotly stuff

--- src/tracker.py
+++ src/tracker.py
@@ -1,17 +1,10 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 
 from datetime import datetime
-import dash
-from dash import dcc
-from dash import html
-import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import pandas as pd
-
 
 from setup import load_data
-from Stats import Stats
+from myPlotly.plotly_util import *
+from myPlotly.aggregate_commit_count import create_aggregate_commit_count_plot
 #from HairyPlotter import HairyPlotter
 
 
@@ -40,39 +33,15 @@ if __name__ == "__main__":
         write_to_pickle = not read_from_pickle
     )
 
-    # create dash instance
-    dash_app = dash.Dash(__name__)
+
+    # generate plotly figs
+    agg_commits_fig = create_aggregate_commit_count_plot(token, token_data_df, project_commits_list)
     
-    # add price plot
-    fig = px.line(token_data_df, x='ms_timestamp', y="close", title=f"Commit Count vs {token} Price")
-    fig.layout.update(yaxis = go.YAxis(title=f"{token} price in USD", side="left"))
+    # plot them
+    dash_app = create_dash()
+    add_layout(dash_app, agg_commits_fig)
+    run_dash(dash_app)
     
-    # add commits over price plot
-    ts_bucket_list, commits_per_bucket = Stats.calculate_commit_count_in_range(project_commits_list, "5min")
-    commits_df = pd.DataFrame({
-        'ms_timestamp': ts_bucket_list,
-        'commits_per_5min_range': commits_per_bucket 
-    })
-
-    # add number of commits for each 5min window
-    fig.add_trace(go.Scatter(
-        mode="markers",
-        x=commits_df["ms_timestamp"],
-        y=commits_df["commits_per_5min_range"],
-        name="Total # Commits",
-        yaxis='y2'))
-    fig.layout.update(yaxis2 = go.YAxis(title="Total # Commits", overlaying='y', side='right'))
-
-    # create the dashboard page
-    dash_app.layout = html.Div([
-        dcc.Graph(
-            id='price-vs-some-metric',
-            figure=fig
-        )
-    ])
-
-    # this needs to run last AFTER you configure the dash_app object layout
-    dash_app.run_server(debug=True)
 
     """
     # matplotlib
